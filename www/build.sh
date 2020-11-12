988d5758c1c66e7d70691cf748211a2a7438071a
iquidus <luke@iquidus.io>
2020-11-12 11:28:51 +0100

    auto patch intl-format-cache from build.sh; update README

--- www/build.sh
+++ www/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-#Test
+# This is dirty af but will do for now.
+cp fix/intl-format-cache/src/* node_modules/intl-format-cache/src/
+cp fix/intl-format-cache/lib/* node_modules/intl-format-cache/lib/
 
 ./node_modules/.bin/ember build --environment production
